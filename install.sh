1ae49f16b1e91caae354835f48c26bb37db7b7bf
minimalus <32616575+minimalus@users.noreply.github.com>
2017-10-14 22:54:55 +0200

    first version

---
+++ install.sh
@@ -0,0 +1,36 @@
+##################
+## install nvim ##
+##################
+if dpkg-query -W neovim; then
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install -y neovim
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
+fi
+
+## install vim-plug ##
+if [ ! -f "~/.local/share/nvim/site/autoload/plug.vim" ]; then
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+#################
+## install zsh ##
+#################
+
+
+
+## install antigen
+sudo apt-get install -y zsh-antigen
+
+##################
+## install tmux ##
+##################
+sudo apt-get install -y tmux

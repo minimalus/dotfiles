5609170addb00da89b264509345f7f1e7abac483
minimalus <32616575+minimalus@users.noreply.github.com>
2017-10-20 20:38:03 +0200

    creating backup cfg file if a previous cfg exists

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# check if script gets executed in dotfiles directory
+if [[ ! -f zsh/zshrc || ! -f tmux/tmux.conf || ! -f neovim/init.vim ]] ; then
+  echo "Could not find all required configs. Maybe you did not call install.sh from the dotfile directory?"
+  exit 1
+fi
+
 # install req. packages
 sudo ./install_pkgs.sh
 
--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 NORMAL=$(tput sgr0)
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
@@ -8,36 +8,37 @@ dotfiles=`pwd`
 lnif() {
   # does requested config exist
   if [ ! -e $1 ] ; then
-    printf "${RED}Failed; (Config $1 not found)${NORMAL}\n"; return;
+    printf "${RED}Failed; (Config $1 not found)${NORMAL}\n"; return
   fi
-  # old config exists already
-  if [ -e $2 ] ; then
-    # config is a symbolic link
-    if [[  -L $2 && "$(readlink -f $1)" && "$1" ]] ; then
-      printf "${GREEN}Link exists already${NORMAL}\n"
-      return
+  
+  # does the correct link already exist
+  if [[ -e $2 && -L $2 && "$(readlink -f $2)" == "$1" ]] ; then  # config exists, it is a link and is equal to requested link
+      printf "${GREEN}Link exists already${NORMAL}\n"; return
+  fi
+
+  # need to check if we need to create a backup
+  if [ -e $2  ] ; then
+    if [ ! -L $2 ] ; then # it is not a link, create backup
+      mv $2 $2_backup
+      printf "${RED}Old config $2 backuped to $2_backup${NORMAL}\n"
+    else # is a link, no backup just remove
+      printf "${RED}Removed old link $2 ${NORMAL}\n"
+      rm $2;
     fi
-    # else ask if we should remove old file or link
-    while true; do
-      read -p "File $2 exists, do you wish to replace it? [y/n]" yn
-      case $yn in
-        [Yy]* ) rm $2;  break;;
-        [Nn]* ) printf "${RED}Skipped${NORMAL}\n";return;;
-        * ) echo "Please answer yes or no.";;
-      esac
-    done
   fi
-  # no previous config found
+  # old config removed or no previous config found
   ln -s $1 $2
   printf "${GREEN}Done${NORMAL}\n"
 }
 
-
+## create symbolic links
 # vim
 printf "Setting up neovim\n"
 lnif $dotfiles/neovim/init.vim $HOME/.config/nvim/init.vim
+# zsh
 printf "Setting up zsh\n"
 lnif $dotfiles/zsh/zshrc $HOME/.zshrc
+# tmux
 printf "Setting up tmux\n"
 lnif $dotfiles/tmux/tmux.conf $HOME/.tmux.config
  
